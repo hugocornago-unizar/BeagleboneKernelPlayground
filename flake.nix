c5ddce9203ef8748e2b0a98055337f354a8afb95
Hugo <hugo@zuperito.xyz>
2025-10-09 23:06:37 +0200

    uboot: link MLO to shell directory

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
 										 cp -r ${config.packages.kernel} kernel
 										 chmod -R +w kernel
 										}
+										ln -fs ${config.packages.uboot}/MLO MLO
 										exec zsh
 						'';
 				};
