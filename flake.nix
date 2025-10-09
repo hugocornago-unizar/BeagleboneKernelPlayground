1c00f3d759b82970aab63be34ffc0947d69c43f8
Hugo <hugo@zuperito.xyz>
2025-10-09 19:43:48 +0200

    flake: modularize using flake-parts

--- flake.nix
+++ flake.nix
@@ -3,70 +3,49 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-    pkgsCross = import nixpkgs {
-      system = "x86_64-linux";
-      crossSystem = {config = "armv7l-unknown-linux-gnueabihf";};
-    };
+	outputs = inputs@{ flake-parts, ... }:
+		flake-parts.lib.mkFlake { inherit inputs; } {
+			systems = [ "x86_64-linux" "aarch64-darwin" ];
+			perSystem = { config, pkgs, system, ...}:
+			let
+				pkgsCross = import inputs.nixpkgs {
+					localSystem = system;
+					crossSystem = { config = "armv7l-unknown-linux-gnueabihf"; };
+				};
+			in
+			{
+				packages.kernel = pkgs.callPackage ./nix/packages/kernel.nix {};
+				packages.uboot = pkgs.callPackage ./nix/packages/uboot.nix { pkgsCross = pkgsCross; };
 
-    kernel-sources = pkgs.stdenv.mkDerivation {
-      name = "kernel-sources";
-      src = pkgs.fetchzip {
-        url = "https://mirrors.edge.kernel.org/pub/linux/kernel/v6.x/linux-6.5.2.tar.xz";
-        hash = "sha256-nXEtRgQZPX76aYpjYUcmBo+VrlPeyIMKyFP0tQIgHZM=";
-      };
+				devShells.default = pkgs.mkShell {
+					buildInputs = with pkgs;
+						[
+							pkg-config
+							ncurses
+							qt5.qtbase
+							pkgsCross.gcc9Stdenv.cc
+							(hiPrio gcc9)
+						]
+						++ pkgs.linux.nativeBuildInputs;
 
-      rtpatch = pkgs.fetchurl {
-        url = "https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.5/patch-6.5.2-rt8.patch.xz";
-        hash = "sha256-9zFDuMnH1t6nSnrEONqFe8G7bwiq32F8z7pGTyspISo=";
-      };
-
-      phases = ["unpackPhase" "buildPhase" "installPhase"];
-
-      buildPhase = ''
-        # apply rtpatch
-        xzcat $rtpatch | patch -p1
-      '';
-
-      installPhase = ''
-        mkdir -p $out
-        cp -r * $out
-      '';
-    };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs;
-        [
-          pkg-config
-          ncurses
-          qt5.qtbase
-          # new gcc usually causes issues with building kernel so use an old
-          # one
-          pkgsCross.gcc9Stdenv.cc
-          (hiPrio gcc9)
-        ]
-        ++ pkgs.linux.nativeBuildInputs;
-
-      shellHook = ''
-              export ARCH=arm
-              export hardeningDisable=all
-              export CROSS_COMPILE=armv7l-unknown-linux-gnueabihf-
-              export PKG_CONFIG_PATH="${pkgs.ncurses.dev}/lib/pkgconfig:${pkgs.qt5.qtbase.dev}/lib/pkgconfig"
-              export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins"
-              export QT_QPA_PLATFORMTHEME=qt5ct
-							[[ -d ./kernel ]] || {
-							 cp -r ${kernel-sources} kernel
-							 chmod -R +w kernel
-							}
-              exec zsh
-      '';
-    };
-  };
+						packages = [ config.packages.uboot ];
+						shellHook = ''
+										export ARCH=arm
+										export hardeningDisable=all
+										export CROSS_COMPILE=armv7l-unknown-linux-gnueabihf-
+										export PKG_CONFIG_PATH="${pkgs.ncurses.dev}/lib/pkgconfig:${pkgs.qt5.qtbase.dev}/lib/pkgconfig"
+										export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins"
+										export QT_QPA_PLATFORMTHEME=qt5ct
+										[[ -d ./kernel ]] || {
+										 cp -r ${config.packages.kernel} kernel
+										 chmod -R +w kernel
+										}
+										exec zsh
+						'';
+				};
+			};
+		};
 }
---
+++ nix/packages/uboot.nix
@@ -0,0 +1,42 @@
+{ pkgs, pkgsCross }:
+pkgs.stdenv.mkDerivation(finalAttrs: {
+	name = "uboot";
+	src = pkgs.fetchFromGitHub {
+		owner = "u-boot";
+		repo = "u-boot";
+		tag = "v2023.10";
+		hash = "sha256-f0xDGxTatRtCxwuDnmsqFLtYLIyjA5xzyQcwfOy3zEM=";
+	};
+
+	buildInputs = with pkgs; [ 
+		flex
+		bison
+		openssl
+		bc
+		pkgsCross.gcc9Stdenv.cc
+		(hiPrio gcc9)
+	];
+
+	dontFixup = true;
+
+	buildPhase = ''
+		runHook preBuild
+
+		export hardeningDisable=all
+		export CROSS_COMPILE=armv7l-unknown-linux-gnueabihf-
+		export DEVICE_TREE=am335x-boneblack
+		make -j$(nproc) am335x_evm_defconfig
+		make -j$(nproc)
+
+		runHook postBuild
+	'';
+
+	installPhase = ''
+		runHook preInstall
+
+		mkdir -p $out
+		cp MLO $out
+
+		runHook postInstall
+	'';
+})
